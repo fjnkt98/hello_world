1a2253615d158d2a87b0c8a465d9ebe4ded955ab
fjnkt98 <a19613@g.ichinoseki.ac.jp>
2019-11-11 21:08:12 +0900

    Update: divided node implementation into header file and source file

---
+++ hello_world_cpp/include/hello_world_cpp/talker.hpp
@@ -0,0 +1,24 @@
+#ifndef HELLO_WORLD_CPP__TALKER_HPP
+#define HELLO_WORLD_CPP__TALKER_HPP
+
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/string.hpp>
+
+namespace hello_world_cpp{
+  class Talker : public rclcpp::Node{
+    public:
+      HELLO_WORLD_CPP_PUBLIC
+
+      explicit Talker(const rclcpp::NodeOptions &options);
+
+    private:
+      size_t count_;
+      std_msgs::msg::String msg_;
+      rclcpp::Publisher<std_msgs::msg::String>::SharedPtr pub_;
+      rclcpp::TimerBase::SharedPtr timer_;
+
+      void publish_message();
+  };
+} // namespace hello_world_cpp
+
+#endif // HELLO_WORLD_CPP__TALKER_HPP
