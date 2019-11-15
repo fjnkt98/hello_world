682e5d27024ac7fd279da54091f6fcb868c769fc
fjnkt98 <a19613@g.ichinoseki.ac.jp>
2019-11-15 09:35:20 +0900

    Update: implement service handler as member function

--- hello_world_cpp/src/server.cpp
+++ hello_world_cpp/src/server.cpp
@@ -18,8 +18,10 @@ public:
   explicit ServerNode(const rclcpp::NodeOptions & options)
   : Node("add_two_ints_server", options)
   {
+    using namespace std::placeholders;
+    
     setvbuf(stdout, NULL, _IONBF, BUFSIZ);
-    auto handle_add_two_ints =
+    /*auto handle_add_two_ints =
       [this](const std::shared_ptr<rmw_request_id_t> request_header,
         const std::shared_ptr<hello_world_msgs::srv::AddTwoInts::Request> request,
         std::shared_ptr<hello_world_msgs::srv::AddTwoInts::Response> response) -> void
@@ -28,13 +30,21 @@ public:
         RCLCPP_INFO(this->get_logger(), "Incoming request\na: %" PRId64 " b: %" PRId64,
           request->a, request->b);
         response->sum = request->a + request->b;
-      };
+      };*/
     // Create a service that will use the callback function to handle requests.
-    srv_ = create_service<hello_world_msgs::srv::AddTwoInts>("add_two_ints", handle_add_two_ints);
+    srv_ = create_service<hello_world_msgs::srv::AddTwoInts>("add_two_ints", std::bind(&ServerNode::handle_add_two_ints, this, _1, _2, _3));
   }
 
 private:
   rclcpp::Service<hello_world_msgs::srv::AddTwoInts>::SharedPtr srv_;
+  void handle_add_two_ints(const std::shared_ptr<rmw_request_id_t> request_header,
+        const std::shared_ptr<hello_world_msgs::srv::AddTwoInts::Request> request,
+        std::shared_ptr<hello_world_msgs::srv::AddTwoInts::Response> response)
+  {
+    (void)request_header;
+    RCLCPP_INFO(this->get_logger(), "Incoming request\na: %" PRId64 " b: %" PRId64, request->a, request->b);
+    response->sum = request->a + request->b;
+  }
 };
 
 }  // namespace hello_world_cpp
