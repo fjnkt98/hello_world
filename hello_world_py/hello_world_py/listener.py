68a799c887a74b198677425ad611e34220b87b73
fjnkt98 <a19613@g.ichinoseki.ac.jp>
2020-11-20 16:57:01 +0900

    Update: リファクタリング

--- hello_world_py/hello_world_py/listener.py
+++ hello_world_py/hello_world_py/listener.py
@@ -1,30 +1,39 @@
 import rclpy
-from rclpy.node import Node
+import rclpy.node
+import rclpy.qos
+import std_msgs.msg
 
-from std_msgs.msg import String
 
-class Listener(Node):
+class Listener(rclpy.node.Node):
+
     def __init__(self):
         super().__init__('listener')
+
+        # Create subscription with topic name 'chatter'
+        qos = rclpy.qos.QoSProfile(depth=10)
         self._subscription = self.create_subscription(
-                String,
+                std_msgs.msg.String,
                 'chatter',
                 self.listener_callback,
-                10)
-        self._subscription
+                qos)
+        # self._subscription
 
     def listener_callback(self, msg):
         self.get_logger().info('I heard: "%s"' % msg.data)
 
+
 def main(args=None):
+    # Node initialization
     rclpy.init(args=args)
 
-    try:
-        listener = Listener()
-        rclpy.spin(listener)
-    finally:
-        listener.destroy_node()
-        rclpy.shutdown()
+    # Create node object and spin
+    listener = Listener()
+    rclpy.spin(listener)
+
+    # Shutting down the node
+    listener.destroy_node()
+    rclpy.shutdown()
+
 
 if __name__ == '__main__':
     main()
--- hello_world_py/hello_world_py/talker.py
+++ hello_world_py/hello_world_py/talker.py
@@ -1,31 +1,48 @@
 import rclpy
-from rclpy.node import Node
+import rclpy.node
+import rclpy.qos
+import std_msgs.msg
 
-from std_msgs.msg import String
 
-class Talker(Node):
+class Talker(rclpy.node.Node):
+
     def __init__(self):
         super().__init__('talker')
-        self._publisher = self.create_publisher(String, 'chatter', 10)
+
+        # Create publisher with topic name 'chatter'
+        # QoS profile: depth = 10
+        qos = rclpy.qos.QoSProfile(depth=10)
+        self._publisher = self.create_publisher(
+                std_msgs.msg.String,
+                'chatter',
+                qos)
+
+        # Create timer with period 0.5 [seconds]
         self._timer = self.create_timer(0.5, self.timer_callback)
+
+        # Increment variable
         self._i = 0
 
     def timer_callback(self):
-        msg = String()
+        msg = std_msgs.msg.String()
         msg.data = 'Hello World: %d' % self._i
         self._publisher.publish(msg)
         self.get_logger().info('Publishing: "%s"' % msg.data)
         self._i += 1
 
+
 def main(args=None):
+    # Node initialization
     rclpy.init(args=args)
 
-    try:
-        talker = Talker()
-        rclpy.spin(talker)
-    finally:
-        talker.destroy_node()
-        rclpy.shutdown()
+    # Create node object and spin
+    talker = Talker()
+    rclpy.spin(talker)
+
+    # Shutting down the node
+    talker.destroy_node()
+    rclpy.shutdown()
+
 
 if __name__ == '__main__':
     main()
